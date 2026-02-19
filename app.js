c7bd0281198eacc6e206bbcf71fe15fc782ff16b
dmjfallon <dmjfallon@gmail.com>
2026-02-19 18:38:28 -0300

    change defaults make table breakdown more obvious

--- app.js
+++ app.js
@@ -249,7 +249,7 @@ function preloadDefaults() {
 
   document.getElementById("m1-balance").value = 150000;
   document.getElementById("m1-rate").value = 5.1;
-  document.getElementById("m1-years").value = 14;
+  document.getElementById("m1-years").value = 11;
   document.getElementById("m1-months").value = 5;
   document.getElementById("m1-extra").value = 250;
 
@@ -257,7 +257,7 @@ function preloadDefaults() {
   document.getElementById("m2-rate").value = 4.5;
   document.getElementById("m2-years").value = 25;
   document.getElementById("m2-months").value = 0;
-  document.getElementById("m2-extra").value = 500;
+  document.getElementById("m2-extra").value = 100;
 
   document.getElementById("redirect-scheduled").checked = true;
   document.getElementById("redirect-extra").checked = true;
